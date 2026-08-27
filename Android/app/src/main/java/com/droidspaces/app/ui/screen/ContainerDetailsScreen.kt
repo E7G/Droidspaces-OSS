c70c8eb5a7c33466e83afc46af67bd1600c38eba
E7G <55611737+E7G@users.noreply.github.com>
2026-08-27 23:13:28 +0800

    feat(ui): surface Anland Linux apps on container details

--- Android/app/src/main/java/com/droidspaces/app/ui/screen/ContainerDetailsScreen.kt
+++ Android/app/src/main/java/com/droidspaces/app/ui/screen/ContainerDetailsScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.droidspaces.app.ui.component.ContainerUsersCard
+import com.droidspaces.app.ui.component.AnlandAppsCard
 import com.droidspaces.app.util.ContainerInfo
 import com.droidspaces.app.util.ContainerOSInfoManager
 import com.droidspaces.app.util.ContainerSystemdManager
@@ -273,6 +274,12 @@ fun ContainerDetailsScreen(
                         anlandSocket?.let { AnlandUtils.launchWindow(context, container.name, it) }
                     }
                 )
+            }
+
+            if (container.enableAnland && anlandSocket != null) {
+                item(key = "anland_apps_${container.name}") {
+                    AnlandAppsCard(containerName = container.name)
+                }
             }
 
             item(key = "systemd_${container.name}") {
